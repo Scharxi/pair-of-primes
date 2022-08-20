1fabe16620a3ed1ad28822c65cd287148e2ec73f
BufferOverflow <scharxidev@outlook.de>
2022-08-20 14:06:47 +0200

    made program async

--- PairOfPrimes/Program.cs
+++ PairOfPrimes/Program.cs
@@ -1,4 +1,4 @@
-﻿void SieveOfEratosthenes(uint n, IList<bool> isPrime)
+﻿Task SieveOfEratosthenes(uint n, IList<bool> isPrime)
 {
     // There are no prime numbers below one and one it self is also not a prime number
     isPrime[0] = isPrime[1] = false;
@@ -12,15 +12,17 @@
         for (var i = p * p; i <= n; i += p)
             isPrime[i] = false;
     }
+
+    return Task.CompletedTask;
 }
 
 
-int FindPrimePair(uint n)
+async Task<int> FindPrimePair(uint n)
 {
     // create an array of size n + 1
     var isPrime =  new bool[n + 1] ;
     var solutions = 0;
-    SieveOfEratosthenes(n, isPrime);
+    await SieveOfEratosthenes(n, isPrime);
     
     for (var i = 0; i < n; i++)
     {
@@ -49,7 +51,7 @@ if (num != null)
         return;
     }
     
-    var n = FindPrimePair(number);
+    var n = await FindPrimePair(number);
     
     Console.WriteLine($"\nFound {n} solutions for number {number}");
     Environment.Exit(0);
